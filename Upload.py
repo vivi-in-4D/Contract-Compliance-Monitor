d318990e63d8049644ac63e748a68417e9ac40ee
Kertoga <80853355+Kertoga@users.noreply.github.com>
2025-05-08 11:45:17 -0500

    Fixed Upload.py

--- Upload.py
+++ Upload.py
@@ -1,8 +1,5 @@
 import pymysql
 import sys
-if len(sys.argv) != 7:
-    print("Usage: python DatabaseTest.py <IP> <IV> <CUI_NAME> <PRE_HASH> <POST_HASH> <GROUP_NAME>")
-    sys.exit(1)
 
 ip, iv, cui_name, pre_hash, post_hash, group_name = sys.argv[1:7]
 
@@ -20,13 +17,15 @@ result = cursor.fetchone()
 
 if result[0] > 0:
     # IF CUI_NAME EXISTS, UPDATE THE IV
-    cursor.execute(f"UPDATE `{group_name}_cui` SET `IV` = '{iv}' WHERE `cui_name` = '{cui_name}';")
+    cursor.execute(f"UPDATE {group_name}_cui SET IV = {iv} WHERE cui_name = '{cui_name}';")
+    connection.commit()
 
 else:
-    #INSERTS NEW CUI INTO THE DATABASE
     cursor.execute(f"INSERT INTO `{group_name}_cui` (`cui_name`, `IV`) VALUES ('{cui_name}', '{iv}');")
-cursor.execute(f"INSERT INTO `c_hashes` (`cui_hash`) VALUES ('{pre_hash}');")
-cursor.execute(f"INSERT INTO `c_hashes` (`cui_hash`) VALUES ('{post_hash}');")
+    connection.commit()
+cursor.execute(f"INSERT INTO c_hashes (cui_hash) VALUES ('{pre_hash}');")
+cursor.execute(f"INSERT INTO c_hashes (cui_hash) VALUES ('{post_hash}');")
+connection.commit()
 
 cursor.close()
 connection.close()
